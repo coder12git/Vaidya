e0a322d2af482b062cbf1451d6b3c04b58a36a34
Suman Khan <sumankhan24649@gmail.com>
2025-02-22 15:57:55 +0530

    video demo added

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import "../styles/Contact.css";
+import { FaUser, FaEnvelope, FaPhone, FaPaperPlane } from "react-icons/fa";
 
-const Contact = () => {
+const ContactUs = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="contact-section" id="contact">
+      <h2>Connect with Us</h2>
+      <p>Connect Your number with us such that You can be benifiited by this medical Assistant!</p>
 
-export default Contact
+      <div className="contact-container">
+        <form className="contact-form">
+          <div className="input-group">
+            <FaUser className="icon" />
+            <input type="text" placeholder="Your Name" required />
+          </div>
+
+          <div className="input-group">
+            <FaEnvelope className="icon" />
+            <input type="email" placeholder="Your Email" required />
+          </div>
+
+          <div className="input-group">
+            <FaPhone className="icon" />
+            <input type="tel" placeholder="Your Phone Number" required />
+          </div>
+
+          <button type="submit" className="send-btn">
+           Connect Number<FaPaperPlane />
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactUs;
