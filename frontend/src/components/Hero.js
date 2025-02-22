2dd0fa02b44bab2f86256a7c0ab1b9f5f7b30611
Souvik Saha <souviksaha7789@gmail.com>
2025-02-22 14:50:57 +0530

    added Hero Section

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import "../styles/Hero.css";
+import medicalVideo from "../assets/Medical3.mp4";
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="hero" id="Hero">
+     
+        <div className="hero-content">
+          <h1>Your Medical Assistant, Anytime</h1>
+          <p>Get instant medical help via call & SMS with Sahayak.</p>
+          <a href="tel:+1234567890">
+            <button className="cta-button">Call Assistance    (+1234567890)</button>
+          </a>
+        </div>
+        <div className="hero-video">
+          <video src={medicalVideo} autoPlay loop muted playsInline className="video" />
+        </div>
+ 
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
