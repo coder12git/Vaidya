e0a09f26755accc842a6850eee79b4335d018427
Suman Khan <sumankhan24649@gmail.com>
2025-02-22 17:49:44 +0530

    footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import FAQ from "./components/FAQ";
 import Team from "./components/Team";
 import VideoDemo from "./components/VideoDemo";
 import Flowchart from "./components/Flowchart";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <FAQ/>
       <Contact />
       <Team/>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styles/Footer.css";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        {/* Logo */}
+        <div className="footer-logo">Sahayak</div>
+
+        {/* Social Media Icons */}
+        <div className="social-icons">
+          <a href="#"><FaFacebook /></a>
+          <a href="#"><FaTwitter /></a>
+          <a href="#"><FaInstagram /></a>
+          <a href="#"><FaLinkedin /></a>
+        </div>
+
+        {/* Copyright */}
+        <div className="footer-bottom">
+          <p>© {new Date().getFullYear()} Sahayak. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
