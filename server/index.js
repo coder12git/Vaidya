b773ab24b361ccc139b9255ea965b209be6feaee
coder12git <suruchikumarimfp4@gamil.com>
2025-02-23 12:13:26 +0530

    added call logs
    Signed-off-by: coder12git <suruchikumarimfp4@gamil.com>

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 // main server
 require("dotenv").config();
-
 const express = require ('express');
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const twilio = require('twilio');
 const PORT = process.env.PORT || 8000;
+
+const accountSID = process.env.TWILIO_ACCOUNT_SID;
+const authToken= process.env.TWILIO_AUTH_TOKEN;
+
+const client = twilio(accountSID, authToken);
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
@@ -17,6 +23,44 @@ app.use('/sms', textRoutes);
 // // Use call router
 const callRoutes = require('./routes/call.routes');
 app.use('/voice', callRoutes);
+
+// Call logs
+app.get('/call-logs', async (req, res) => {
+    try {
+        const calls = await client.calls.list({ limit: 50})// Fetch last 50 calls
+        const callLogs = calls.map(call => ({
+            dateTime: call.dateCreated.toLocaleString(),
+            phoneNumber: call.from,
+            serviceType: 'Voice Call', 
+            duration: call.duration,
+            responseSummary: call.status,
+        }));
+
+        res.json(callLogs);
+    } catch (error) {
+        console.error('Error fetching call logs:', error);
+        res.status(500).json({ error: 'Failed to fetch call logs' });
+    }
+});
+
+// SMS logs
+app.get('/sms-logs', async (req, res) => {
+    try {
+        const calls = await client.calls.list({ limit: 50});
+        const callLogs = calls.map(call => ({
+            dateTime: call.dateCreated.toLocaleString(),
+            phoneNumber: call.from,
+            serviceType: 'SMS', 
+            duration: call.duration,
+            responseSummary: call.status, 
+        }));
+
+        res.json((callLogs));
+    } catch (error) {
+        console.error('Error fetching call logs:', error);
+        res.status(500).json({ error: 'Failed to fetch call logs' });
+    }
+});
 
 app.get("/", (req,res)=>{
     res.json({ message: "Welcome to Sahayak, A Gen AI-based medical assistant that works without the internet, accessible via a simple phone call. To use the service, please call us at the given number: +19283796252" });
